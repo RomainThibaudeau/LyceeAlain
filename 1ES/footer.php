33591bcdb02ef419a374129b771abc01db51b8d1
RomainThibaudeau <137986062+RomainThibaudeau@users.noreply.github.com>
2025-04-27 08:35:29 +0200

    Update footer.php

--- 1ES/footer.php
+++ 1ES/footer.php
@@ -1,8 +1,8 @@
- 
+  
 
     <footer>
         <div class="container">
-            <p> Physique chimie 2024-2025 </p>
+            <p> Physique chimie 2024-2025 testkkdk,dk,ddf </p>
           <p> Lycée Alain - R. Thibaudeau</p>
         </div>
     </footer>
